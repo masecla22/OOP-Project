edd862c14380aa3446a25d60fcf4d7742f3cd6d2
masecla22 <supermasecla22@gmail.com>
2023-05-19 15:48:22 +0200

    Update Item.java
    Javadoc

--- 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
+++ 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
@@ -5,11 +5,15 @@ import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * An item that can be picked up and used.
+ */
 @Data
 @AllArgsConstructor
 public abstract class Item implements Serializable {
     /** Serial version ID. */
     private static final long serialVersionUID = 2145629086987l;
+
     private String name;
     private String description;
 }
