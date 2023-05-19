ea21b07576ef380c51bc0d83ef356dff3d1e126f
masecla22 <supermasecla22@gmail.com>
2023-05-19 15:47:26 +0200

    Door explicit constructor + javadoc

--- 2_rpg/src/main/java/nl/rug/oop/rpg/game/objects/Door.java
+++ 2_rpg/src/main/java/nl/rug/oop/rpg/game/objects/Door.java
@@ -1,8 +1,8 @@
 package nl.rug.oop.rpg.game.objects;
 
-import lombok.AllArgsConstructor;
+import java.io.Serializable;
+
 import lombok.Data;
-import lombok.NonNull;
 import nl.rug.oop.rpg.game.behaviours.Inspectable;
 import nl.rug.oop.rpg.game.behaviours.Interactable;
 import nl.rug.oop.rpg.game.player.Player;
@@ -11,20 +11,34 @@ import nl.rug.oop.rpg.game.player.Player;
  * A door is an object that can be interacted with and inspected.
  */
 @Data
-@AllArgsConstructor
 public class Door implements Inspectable, Interactable, Serializable {
     /** Serial version ID. */
     private static final long serialVersionUID = 2140956097087l;
 
     /** The description of the door. */
-    @NonNull
     private String description;
 
-    /** The room the door comes from */
+    /** The room the door comes from. */
     private Room source;
 
     /** The room the door leads to. */
     private Room destination;
+
+    /**
+     * Creates a new door. The constructor will also add the door to the source
+     * room.
+     * 
+     * @param description - the description of the door
+     * @param source      - the room the door comes from
+     * @param destination - the room the door leads to
+     */
+    public Door(String description, Room source, Room destination) {
+        this.description = description;
+        this.source = source;
+        this.destination = destination;
+
+        source.addDoor(this);
+    }
 
     /**
      * Inspects the door.
