e8e10881c6e9de83fa241d02ea482c41af73c2fd
masecla22 <supermasecla22@gmail.com>
2023-05-19 15:48:17 +0200

    Item is now serializable

--- 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
+++ 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
@@ -1,11 +1,15 @@
 package nl.rug.oop.rpg.game.items;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public abstract class Item {
+public abstract class Item implements Serializable {
+    /** Serial version ID. */
+    private static final long serialVersionUID = 2145629086987l;
     private String name;
     private String description;
 }
