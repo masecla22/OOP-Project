de19c71fa4654a238e622304f09953943d2e91ff
denineagu <denisa.neagu.03@gmail.com>
2023-06-04 18:40:41 +0200

    Made item copyable

--- 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
+++ 2_rpg/src/main/java/nl/rug/oop/rpg/game/items/Item.java
@@ -4,16 +4,20 @@ import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import nl.rug.oop.rpg.game.behaviours.Copyable;
 
 /**
  * An item that can be picked up and used.
  */
 @Data
 @AllArgsConstructor
-public abstract class Item implements Serializable {
+public abstract class Item implements Serializable, Copyable {
     /** Serial version ID. */
     private static final long serialVersionUID = 2145629086987l;
 
     private String name;
     private String description;
+
+    @Override
+    public abstract Item copy();
 }
