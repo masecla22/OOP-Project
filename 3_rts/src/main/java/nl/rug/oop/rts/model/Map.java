d186e5e1fefe9ab42483afc744e2865cd0481808
masecla22 <supermasecla22@gmail.com>
2023-06-07 18:16:31 +0200

    Update Map.java

--- 3_rts/src/main/java/nl/rug/oop/rts/model/Map.java
+++ 3_rts/src/main/java/nl/rug/oop/rts/model/Map.java
@@ -1,10 +1,14 @@
 package nl.rug.oop.rts.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import lombok.Getter;
+
+@Getter
 public class Map {
-    private Set<Node> nodes;
-    private Set<Edge> edges;
+    private Set<Node> nodes = new HashSet<>();
+    private Set<Edge> edges = new HashSet<>();
 
     public void addNode(Node node) {
         nodes.add(node);
@@ -22,6 +26,9 @@ public class Map {
 
     public void addEdge(Edge edge) {
         edges.add(edge);
+
+        edge.getPointA().getEdges().add(edge);
+        edge.getPointB().getEdges().add(edge);
     }
 
     public void removeEdge(Edge edge) {
