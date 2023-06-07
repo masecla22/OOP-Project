1f68f44f9cb5d6864950ef783468965aebc95d4b
masecla22 <supermasecla22@gmail.com>
2023-06-07 16:05:19 +0200

    Create SingleplayerView.java

---
+++ 3_rts/src/main/java/nl/rug/oop/rts/view/settings/SettingsView.java
@@ -0,0 +1,20 @@
+package nl.rug.oop.rts.view.settings;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import nl.rug.oop.rts.Game;
+
+public class SettingsView extends JPanel {
+
+    private Game game;
+
+    public SettingsView(Game game) {
+        this.game = game;
+
+        
+    }
+
+
+
+}
