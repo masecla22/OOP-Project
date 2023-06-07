7f1c7fe55f1366376d1afcc4f437fbb4e958a21f
masecla22 <supermasecla22@gmail.com>
2023-06-07 18:15:01 +0200

    Update SingleplayerView.java

--- 3_rts/src/main/java/nl/rug/oop/rts/view/singleplayer/SingleplayerView.java
+++ 3_rts/src/main/java/nl/rug/oop/rts/view/singleplayer/SingleplayerView.java
@@ -1,7 +1,12 @@
 package nl.rug.oop.rts.view.singleplayer;
 
-import javax.swing.JPanel;
+import nl.rug.oop.rts.Game;
+import nl.rug.oop.rts.model.Map;
+import nl.rug.oop.rts.view.game.GameView;
 
-public class SingleplayerView extends JPanel {
-
+public class SingleplayerView extends GameView {
+    public SingleplayerView(Game game, Map map) {
+        super(game, map);
+    }
+    
 }
