cc90c41e334dfa8f101d4de35690e2993edf3f8a
masecla22 <supermasecla22@gmail.com>
2023-06-07 18:16:30 +0200

    Update Node.java

--- 3_rts/src/main/java/nl/rug/oop/rts/model/Node.java
+++ 3_rts/src/main/java/nl/rug/oop/rts/model/Node.java
@@ -1,18 +1,27 @@
 package nl.rug.oop.rts.model;
 
+import java.awt.Point;
+import java.util.HashSet;
 import java.util.Set;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @EqualsAndHashCode(of = "id")
+@RequiredArgsConstructor
 public class Node {
-    private int id;
+    public static final int NODE_SIZE = 64;
 
+    private final int id;
+
+    @NonNull
+    private Point position;
+
+    @NonNull
     private String name;
 
-    private Set<Edge> edges;
-
-    
+    private Set<Edge> edges = new HashSet<>();
 }
