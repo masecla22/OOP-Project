b09cea81101eb410de7eb62429f018c0580e5054
masecla22 <supermasecla22@gmail.com>
2023-06-08 03:53:09 +0200

    Added button for edge

--- 3_rts/src/main/java/nl/rug/oop/rts/view/game/GameView.java
+++ 3_rts/src/main/java/nl/rug/oop/rts/view/game/GameView.java
@@ -3,15 +3,16 @@ package nl.rug.oop.rts.view.game;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import nl.rug.oop.rts.Game;
+import nl.rug.oop.rts.interfaces.observing.Observer;
 import nl.rug.oop.rts.model.Edge;
 import nl.rug.oop.rts.model.Map;
 import nl.rug.oop.rts.model.Node;
-import nl.rug.oop.rts.observing.Observer;
 import nl.rug.oop.rts.view.map.MapView;
 
 @Getter(AccessLevel.PROTECTED)
@@ -31,17 +32,28 @@ public abstract class GameView extends JPanel implements Observer {
 
         this.map.addObserver(this);
 
-        MapView mapView = new MapView(game, map);
+        MapView mapView = new MapView(map);
         this.add(mapView, BorderLayout.CENTER);
     }
 
     @Override
     public void update() {
-        if (this.map.getSelectedNode() == null) {
+        if (this.map.getSelection() == null || !(this.map.getSelection() instanceof Node)) {
+            this.removeNodeButton.setEnabled(false);
+            this.addEdgeButton.setEnabled(false);
+        } else {
+            this.removeNodeButton.setEnabled(true);
+            this.addEdgeButton.setEnabled(true);
+        }
+
+        if (this.map.getSelection() == null || !(this.map.getSelection() instanceof Edge)) {
             this.removeEdgeButton.setEnabled(false);
         } else {
             this.removeEdgeButton.setEnabled(true);
         }
+
+        if (!this.map.isAddingEdge())
+            this.addEdgeButton.setText("Add Edge");
 
         this.repaint();
     }
@@ -64,16 +76,49 @@ public abstract class GameView extends JPanel implements Observer {
         topBar.add(backButton);
 
         topBar.add(addEdgeButton);
+        addEdgeButton.addActionListener(e -> {
+            this.getMap().markAddingEdge();
+            this.getMap().getAddingEdge().thenAccept(endNode -> {
+                Node nodeA = (Node) this.getMap().getSelection();
+                Node nodeB = endNode;
+
+                if (!nodeA.equals(nodeB)) {
+                    if (nodeA != null && nodeB != null) {
+                        this.addEdge(nodeA, nodeB);
+                    }
+                }
+
+                this.getMap().unmarkAddingEdge();
+                this.getMap().setSelection(null);
+            });
+
+            addEdgeButton.setText("Select a second node...");
+        });
+
+        addEdgeButton.setEnabled(false);
+
         topBar.add(removeEdgeButton);
 
         topBar.add(addNodeButton);
+        addNodeButton.addActionListener(e -> {
+            this.createNode(JOptionPane.showInputDialog("What is the name of the node?"));
+        });
+
         topBar.add(removeNodeButton);
 
+        removeNodeButton.setEnabled(false);
         removeNodeButton.addActionListener(e -> {
-            this.removeNode(this.map.getSelectedNode());
+            if (this.map.getSelection() instanceof Node selectedNode)
+                this.removeNode(selectedNode);
+            this.map.setSelection(null);
         });
 
         removeEdgeButton.setEnabled(false);
+        removeEdgeButton.addActionListener(e -> {
+            if (this.map.getSelection() instanceof Edge selectedEdge)
+                this.removeEdge(selectedEdge);
+            this.map.setSelection(null);
+        });
 
         return topBar;
     }
