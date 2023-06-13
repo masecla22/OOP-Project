2b991153f88605fa5bff94dbd56e2649e94f2ed0
masecla22 <supermasecla22@gmail.com>
2023-06-14 00:20:01 +0200

    Removed naming strategies for now

--- 3_rts/rugson/src/main/java/nl/rug/oop/rugson/fields/FieldNamingStrategy.java
+++
@@ -1,9 +0,0 @@
-package nl.rug.oop.rugson.fields;
-
-import java.lang.reflect.Field;
-
-public interface FieldNamingStrategy {
-    String getJsonName(Field field);
-
-    Field getClassField(Class<?> clazz, String jsonName) throws NoSuchFieldException, SecurityException;
-}
