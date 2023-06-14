ab18010bcc6425f827c89ec6c6b0912ac3295043
masecla22 <supermasecla22@gmail.com>
2023-06-15 00:12:21 +0200

    Login packets

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/authentication/login/LoginRequest.java
@@ -0,0 +1,16 @@
+package nl.rug.oop.rts.protocol.packet.definitions.authentication.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.packet.Packet;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class LoginRequest extends Packet {
+    private String username;
+    private String password;
+}
---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/authentication/login/LoginResponse.java
@@ -0,0 +1,22 @@
+package nl.rug.oop.rts.protocol.packet.definitions.authentication.login;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.packet.Packet;
+import nl.rug.oop.rts.protocol.user.User;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class LoginResponse extends Packet {
+    private boolean success;
+    private String error;
+
+    private UUID token;
+    private User user;
+}
