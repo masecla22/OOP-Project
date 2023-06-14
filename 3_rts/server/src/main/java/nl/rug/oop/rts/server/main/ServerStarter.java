a034a6d7fd6134bf35c621271ac2284afba19100
masecla22 <supermasecla22@gmail.com>
2023-06-14 19:15:17 +0200

    Main function in server is now able to throw exceptions

--- 3_rts/server/src/main/java/nl/rug/oop/rts/server/main/ServerStarter.java
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/main/ServerStarter.java
@@ -1,7 +1,9 @@
 package nl.rug.oop.rts.server.main;
 
+import java.io.IOException;
+
 public class ServerStarter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         RTSServer server = new RTSServer();
         server.start();
     }
