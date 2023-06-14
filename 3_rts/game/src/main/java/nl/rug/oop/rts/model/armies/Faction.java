2dada37e68596f554b1dd1418e1ba86109462cdf
masecla22 <supermasecla22@gmail.com>
2023-06-15 01:17:37 +0200

    Update Faction.java

--- 3_rts/game/src/main/java/nl/rug/oop/rts/model/armies/Faction.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/model/armies/Faction.java
@@ -9,14 +9,21 @@ import nl.rug.oop.rts.model.units.UnitType;
 @Getter
 @AllArgsConstructor
 public enum Faction {
-    MEN("Men", "TeamA", List.of(UnitType.GONDOR_SOLDIER, UnitType.ITHILIEN_RANGER, UnitType.TOWER_GUARD)),
-    ELVES("Elves", "TeamA", List.of(UnitType.LORIEN_WARRIOR, UnitType.MIRKWOOD_ARCHER, UnitType.RIVENDELL_LANCER)),
-    DWARVES("Dwarves", "TeamA", List.of(UnitType.AXE_THROWER, UnitType.GUARDIAN, UnitType.PHALANX)),
-    MORDOR("Mordor", "TeamB", List.of(UnitType.HARADRIM_ARCHER, UnitType.ORC_PIKEMAN, UnitType.ORC_WARRIOR)),
-    ISENGARD("Isengard", "TeamB", List.of(UnitType.URUK_CROSSBOW_MAN, UnitType.URUK_HAI, UnitType.WARG_RIDER));
+    MEN("Men", Team.TEAM_A, "factionMen",
+            List.of(UnitType.GONDOR_SOLDIER, UnitType.ITHILIEN_RANGER, UnitType.TOWER_GUARD)),
+    ELVES("Elves", Team.TEAM_A, "factionElves",
+            List.of(UnitType.LORIEN_WARRIOR, UnitType.MIRKWOOD_ARCHER, UnitType.RIVENDELL_LANCER)),
+    DWARVES("Dwarves", Team.TEAM_A, "factionDwarves",
+            List.of(UnitType.AXE_THROWER, UnitType.GUARDIAN, UnitType.PHALANX)),
+    MORDOR("Mordor", Team.TEAM_B, "factionMordor",
+            List.of(UnitType.HARADRIM_ARCHER, UnitType.ORC_PIKEMAN, UnitType.ORC_WARRIOR)),
+    ISENGARD("Isengard", Team.TEAM_B, "factionIsengard",
+            List.of(UnitType.URUK_CROSSBOW_MAN, UnitType.URUK_HAI, UnitType.WARG_RIDER));
 
     private String niceName;
-    private String team;
+    private Team team;
+
+    private String texture;
 
     private List<UnitType> availableUnits;
 
