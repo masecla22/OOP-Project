057e68f8ecc59d21379c1390ec285afdf8de51a8
masecla22 <supermasecla22@gmail.com>
2023-06-14 19:12:48 +0200

    Create RTSPacketDictionary.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/RTSPacketDictionary.java
@@ -0,0 +1,9 @@
+package nl.rug.oop.rts.protocol.packet.dictionary;
+
+public class RTSPacketDictionary extends PacketDictionary {
+    public RTSPacketDictionary() {
+        super();
+
+        // Register packets here
+    }
+}
