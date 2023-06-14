08de32e972ad987601d14f2a7a1ffc5deb0fe705
masecla22 <supermasecla22@gmail.com>
2023-06-14 19:12:46 +0200

    Create PacketDictionary.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/PacketDictionary.java
@@ -0,0 +1,38 @@
+package nl.rug.oop.rts.protocol.packet.dictionary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nl.rug.oop.rts.protocol.packet.Packet;
+import nl.rug.oop.rts.protocol.packet.definitions.keepalive.KeepAlivePacket;
+
+public class PacketDictionary {
+    private Map<Integer, Class<? extends Packet>> packetDictionary = new HashMap<>();
+    private Map<Class<? extends Packet>, Integer> packetIdDictionary = new HashMap<>();
+
+    public PacketDictionary() {
+        this.registerPacket(KeepAlivePacket.class);
+    }
+
+    public void registerPacket(Class<? extends Packet> packetClass) {
+        int nextPacketId = packetDictionary.size();
+        this.registerPacket(nextPacketId, packetClass);
+    }
+
+    public void registerPacket(int packetId, Class<? extends Packet> packetClass) {
+        packetDictionary.put(packetId, packetClass);
+        packetIdDictionary.put(packetClass, packetId);
+    }
+
+    public Class<? extends Packet> getPacketClass(int packetId) {
+        return packetDictionary.get(packetId);
+    }
+
+    public int getPacketId(Class<? extends Packet> packetClass) {
+        return packetIdDictionary.get(packetClass);
+    }
+
+    public int getPacketId(Packet object) {
+        return this.getPacketId(object.getClass());
+    }
+}
