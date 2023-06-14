f1915abb056235b256ec9d26aaaa5561c292e431
masecla22 <supermasecla22@gmail.com>
2023-06-14 20:06:51 +0200

    Create User.java

---
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/user/User.java
@@ -0,0 +1,26 @@
+package nl.rug.oop.rts.server.user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+
+    private String name;
+    private String password;
+    private int elo;
+
+    public User(ResultSet set) throws SQLException {
+        this.id = set.getInt("id");
+        this.name = set.getString("name");
+        this.password = set.getString("password");
+        this.elo = set.getInt("elo");
+    }
+}
