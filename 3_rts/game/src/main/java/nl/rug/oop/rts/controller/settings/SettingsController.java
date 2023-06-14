b8e16693eedc68e90245a8acfcdfe72c82b5e544
masecla22 <supermasecla22@gmail.com>
2023-06-15 00:24:21 +0200

    Create SettingsController.java
    Co-Authored-By: denineagu <108517806+denineagu@users.noreply.github.com>

---
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/controller/settings/SettingsController.java
@@ -0,0 +1,26 @@
+package nl.rug.oop.rts.controller.settings;
+
+import java.io.File;
+
+import nl.rug.oop.rts.model.settings.GameSettings;
+import nl.rug.oop.rugson.Rugson;
+
+public class SettingsController {
+    private GameSettings settings;
+
+    public SettingsController(Rugson rugson) {
+        this.settings = GameSettings.loadConfiguration(rugson, new File("game.json"));
+    }
+
+    public GameSettings getSettings() {
+        return settings;
+    }
+
+    public void save() {
+        settings.save();
+    }
+
+    public void setCentralServer(String centralServer) {
+        settings.setCentralServer(centralServer);
+    }
+}
