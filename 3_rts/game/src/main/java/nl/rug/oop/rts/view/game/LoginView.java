c9149f21fa932711ceb25e7f830ad0cece0de2d4
denineagu <denisa.neagu.03@gmail.com>
2023-06-14 20:06:51 +0200

    created LoginView with loginOptions

---
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/view/game/LoginView.java
@@ -0,0 +1,73 @@
+package nl.rug.oop.rts.view.game;
+
+import nl.rug.oop.rts.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginView extends JPanel {
+    private Game game;
+
+    public LoginView(Game game) {
+        this.game = game;
+        this.setLayout(new BorderLayout());
+        JPanel loginOptions = new JPanel();
+        loginOptions.setSize(10, 10);
+        loginOptions.setLayout(new GridLayout(4, 2, 10, 20));
+
+        this.add(loginOptions, BorderLayout.CENTER);
+
+        addBackButton();
+        addLayout(loginOptions);
+        addUsername(loginOptions);
+        addPassword(loginOptions);
+        addStartButton();
+    }
+
+    private void addBackButton() {
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(e -> {
+            this.game.handleBack();
+        });
+
+        backButton.setPreferredSize(new Dimension(200, 50));
+        this.add(backButton, BorderLayout.PAGE_START);
+
+    }
+
+    private void addLayout(JPanel loginOptions) {
+        JLabel layoutLabel1 = new JLabel();
+        JLabel layoutLabel2 = new JLabel();
+        loginOptions.add(layoutLabel1);
+        loginOptions.add(layoutLabel2);
+    }
+
+    private void addUsername(JPanel loginOptions) {
+        // this adds a label and a text field
+        JLabel usernameLabel = new JLabel("Username:", SwingConstants.CENTER);
+        JTextField usernameField = new JTextField();
+
+        loginOptions.add(usernameLabel);
+        loginOptions.add(usernameField);
+    }
+
+    private void addPassword(JPanel loginOptions) {
+        // this adds a label and a text field
+        JLabel passwordLabel = new JLabel("Password:", SwingConstants.CENTER);
+        JPasswordField passwordField = new JPasswordField();
+
+        loginOptions.add(passwordLabel);
+        loginOptions.add(passwordField);
+    }
+
+    private void addStartButton() {
+        JButton startButton = new JButton("Start game!");
+        startButton.addActionListener(e -> {
+            //this.game.handleStart();
+        });
+
+        startButton.setPreferredSize(new Dimension(200, 50));
+        this.add(startButton, BorderLayout.PAGE_END);
+
+    }
+}
