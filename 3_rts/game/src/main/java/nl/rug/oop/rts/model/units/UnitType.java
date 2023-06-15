6e4f9676bb65ef7720d531655d5ef2c65ee054e8
masecla22 <supermasecla22@gmail.com>
2023-06-15 13:31:00 +0200

    Added 3 Unit types

--- 3_rts/game/src/main/java/nl/rug/oop/rts/model/units/UnitType.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/model/units/UnitType.java
@@ -24,72 +24,83 @@ import nl.rug.oop.rts.model.units.men.TowerGuard;
 import nl.rug.oop.rts.model.units.mordor.HaradrimArcher;
 import nl.rug.oop.rts.model.units.mordor.OrcPikeman;
 import nl.rug.oop.rts.model.units.mordor.OrcWarrior;
+import nl.rug.oop.rts.model.units.rohan.RohanArcher;
+import nl.rug.oop.rts.model.units.rohan.RohanSpearman;
+import nl.rug.oop.rts.model.units.rohan.RohanSwordsman;
 
 @Getter
 @AllArgsConstructor
 public enum UnitType {
-    AXE_THROWER(AxeThrower.class, Faction.DWARVES, 10.0, 30.0, 60.0, 80.0,
-            List.of("Ralph", "Mickey", "Bob")),
-    GUARDIAN(Guardian.class, Faction.DWARVES, 10.0, 20.0, 40.0, 60.0,
-            List.of("Adolph", "Goofy", "Mario")),
-    PHALANX(Phalanx.class, Faction.DWARVES, 5.0, 10.0, 50.0, 60.0,
-            List.of("Lorenzo", "Greg", "Gabby")),
+        AXE_THROWER(AxeThrower.class, Faction.DWARVES, 10.0, 30.0, 60.0, 80.0,
+                        List.of("Ralph", "Mickey", "Bob")),
+        GUARDIAN(Guardian.class, Faction.DWARVES, 10.0, 20.0, 40.0, 60.0,
+                        List.of("Adolph", "Goofy", "Mario")),
+        PHALANX(Phalanx.class, Faction.DWARVES, 5.0, 10.0, 50.0, 60.0,
+                        List.of("Lorenzo", "Greg", "Gabby")),
 
-    LORIEN_WARRIOR(LorienWarrior.class, Faction.ELVES, 15.0, 30.0, 60.0, 70.0,
-            List.of("Fabiola", "Fernus", "Fabrizio")),
-    MIRKWOOD_ARCHER(MirkwoodArcher.class, Faction.ELVES, 10.0, 30.0, 70.0, 80.0,
-            List.of("Odobasian", "Roberta", "Paulo")),
-    RIVENDELL_LANCER(RivendellLancer.class, Faction.ELVES, 10.0, 40.0, 70.0, 90.0,
-            List.of("Maya", "Karina", "Sebastian")),
+        LORIEN_WARRIOR(LorienWarrior.class, Faction.ELVES, 15.0, 30.0, 60.0, 70.0,
+                        List.of("Fabiola", "Fernus", "Fabrizio")),
+        MIRKWOOD_ARCHER(MirkwoodArcher.class, Faction.ELVES, 10.0, 30.0, 70.0, 80.0,
+                        List.of("Odobasian", "Roberta", "Paulo")),
+        RIVENDELL_LANCER(RivendellLancer.class, Faction.ELVES, 10.0, 40.0, 70.0, 90.0,
+                        List.of("Maya", "Karina", "Sebastian")),
 
-    URUK_CROSSBOW_MAN(UrukCrossbowman.class, Faction.ISENGARD, 5.0, 40.0, 60.0, 90.0,
-            List.of("Angelus", "Pablo", "Petrus")),
-    URUK_HAI(UrukHai.class, Faction.ISENGARD, 10.0, 20.0, 40.0, 80.0,
-            List.of("Lucretiu", "Feriga", "Don")),
-    WARG_RIDER(WargRider.class, Faction.ISENGARD, 7.0, 25.0, 60.0, 90.0,
-            List.of("Cici", "Souvlaki", "Couscous")),
+        URUK_CROSSBOW_MAN(UrukCrossbowman.class, Faction.ISENGARD, 5.0, 40.0, 60.0, 90.0,
+                        List.of("Angelus", "Pablo", "Petrus")),
+        URUK_HAI(UrukHai.class, Faction.ISENGARD, 10.0, 20.0, 40.0, 80.0,
+                        List.of("Lucretiu", "Feriga", "Don")),
+        WARG_RIDER(WargRider.class, Faction.ISENGARD, 7.0, 25.0, 60.0, 90.0,
+                        List.of("Cici", "Souvlaki", "Couscous")),
 
-    GONDOR_SOLDIER(GondorSoldier.class, Faction.MEN, 10.0, 30.0, 80.0, 100.0,
-            List.of("Cerasela", "Cayus", "Gordon")),
-    ITHILIEN_RANGER(IthilienRanger.class, Faction.MEN, 5.0, 40.0, 60.0, 75.0,
-            List.of("Mathew", "Andrei", "Paprika")),
-    TOWER_GUARD(TowerGuard.class, Faction.MEN, 10.0, 35.0, 80.0, 90.0,
-            List.of("Hobo", "Horatiu", "Lawrence")),
+        GONDOR_SOLDIER(GondorSoldier.class, Faction.MEN, 10.0, 30.0, 80.0, 100.0,
+                        List.of("Cerasela", "Cayus", "Gordon")),
+        ITHILIEN_RANGER(IthilienRanger.class, Faction.MEN, 5.0, 40.0, 60.0, 75.0,
+                        List.of("Mathew", "Andrei", "Paprika")),
+        TOWER_GUARD(TowerGuard.class, Faction.MEN, 10.0, 35.0, 80.0, 90.0,
+                        List.of("Hobo", "Horatiu", "Lawrence")),
 
-    HARADRIM_ARCHER(HaradrimArcher.class, Faction.MORDOR, 30.0, 40.0, 50.0, 60.0,
-            List.of("Hercules", "Ignatiu", "Chilli")),
-    ORC_PIKEMAN(OrcPikeman.class, Faction.MORDOR, 10.0, 20.0, 80.0, 110.0,
-            List.of("Dove", "Kyle", "Karen")),
-    ORC_WARRIOR(OrcWarrior.class, Faction.MORDOR, 20.0, 30.0, 70.0, 90.0,
-            List.of("Patricia", "Romin", "Goyle"));
+        HARADRIM_ARCHER(HaradrimArcher.class, Faction.MORDOR, 30.0, 40.0, 50.0, 60.0,
+                        List.of("Hercules", "Ignatiu", "Chilli")),
+        ORC_PIKEMAN(OrcPikeman.class, Faction.MORDOR, 10.0, 20.0, 80.0, 110.0,
+                        List.of("Dove", "Kyle", "Karen")),
+        ORC_WARRIOR(OrcWarrior.class, Faction.MORDOR, 20.0, 30.0, 70.0, 90.0,
+                        List.of("Patricia", "Romin", "Goyle")),
 
-    private Class<? extends Unit> unitClass;
+        ROHAN_ARCHER(RohanArcher.class, Faction.ROHAN, 10.0, 30.0, 60.0, 80.0,
+                        List.of("Ralphael", "MickeyD's", "Boaba")),
+        ROHAN_SPEARMAN(RohanSpearman.class, Faction.ROHAN, 10.0, 20.0, 40.0, 60.0,
+                        List.of("Adolphung", "Goofymus", "MarioKart")),
+        ROHAN_SWORDSMAN(RohanSwordsman.class, Faction.ROHAN, 5.0, 10.0, 50.0, 60.0,
+                        List.of("Lorenzoios", "Gregozio", "Gabbyriela Coxanus"));
 
-    private Faction faction;
+        private Class<? extends Unit> unitClass;
 
-    private double minDamage;
-    private double maxDamage;
+        private Faction faction;
 
-    private double minHealth;
-    private double maxHealth;
+        private double minDamage;
+        private double maxDamage;
 
-    private List<String> availableNames;
+        private double minHealth;
+        private double maxHealth;
 
-    private static Map<UnitType, Constructor<? extends Unit>> cachedConstructors = new HashMap<>();
+        private List<String> availableNames;
 
-    @SneakyThrows
-    public Unit buildUnit(String name, double damage, double health) {
-        return getConstructor().newInstance(name, damage, health);
-    }
+        private static Map<UnitType, Constructor<? extends Unit>> cachedConstructors = new HashMap<>();
 
-    @SneakyThrows
-    private Constructor<? extends Unit> getConstructor() {
-        if (cachedConstructors.containsKey(this)) {
-            return cachedConstructors.get(this);
+        @SneakyThrows
+        public Unit buildUnit(String name, double damage, double health) {
+                return getConstructor().newInstance(name, damage, health);
         }
 
-        Constructor<? extends Unit> constructor = this.unitClass.getConstructor(String.class, Double.TYPE, Double.TYPE);
-        cachedConstructors.put(this, constructor);
-        return constructor;
-    }
+        @SneakyThrows
+        private Constructor<? extends Unit> getConstructor() {
+                if (cachedConstructors.containsKey(this)) {
+                        return cachedConstructors.get(this);
+                }
+
+                Constructor<? extends Unit> constructor = this.unitClass.getConstructor(String.class, Double.TYPE,
+                                Double.TYPE);
+                cachedConstructors.put(this, constructor);
+                return constructor;
+        }
 }
