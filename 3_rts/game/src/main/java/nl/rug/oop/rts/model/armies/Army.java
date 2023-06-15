80b0b722781f351a901ca5ae955652ba1da2b8f0
masecla22 <supermasecla22@gmail.com>
2023-06-15 12:18:14 +0200

    Update Army.java

--- 3_rts/game/src/main/java/nl/rug/oop/rts/model/armies/Army.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/model/armies/Army.java
@@ -3,15 +3,23 @@ package nl.rug.oop.rts.model.armies;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import nl.rug.oop.rts.model.Node;
 import nl.rug.oop.rts.model.units.Unit;
 
 @Data
-@AllArgsConstructor
 public class Army {
     private List<Unit> units = new ArrayList<>();
     private Faction faction;
+
+    public Army(@NonNull List<Unit> units, @NonNull Faction faction) {
+        this.units = units;
+        this.faction = faction;
+    }
+
+    private Node movingToNextStep;
+    private boolean moved = false;
 
     @Override
     public String toString() {
