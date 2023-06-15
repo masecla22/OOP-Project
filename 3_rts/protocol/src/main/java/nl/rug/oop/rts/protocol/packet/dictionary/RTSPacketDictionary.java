c4a00e6ac186cf90792509e8fdc79963fb119a48
masecla22 <supermasecla22@gmail.com>
2023-06-16 00:45:31 +0200

    Registered packets to dictionary

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/RTSPacketDictionary.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/RTSPacketDictionary.java
@@ -4,6 +4,11 @@ import nl.rug.oop.rts.protocol.packet.definitions.authentication.login.LoginRequ
 import nl.rug.oop.rts.protocol.packet.definitions.authentication.login.LoginResponse;
 import nl.rug.oop.rts.protocol.packet.definitions.authentication.register.RegistrationRequest;
 import nl.rug.oop.rts.protocol.packet.definitions.authentication.register.RegistrationResponse;
+import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyCreationRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyCreationResponse;
+import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyDeletionRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyListingRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyListingResponse;
 
 public class RTSPacketDictionary extends PacketDictionary {
     public RTSPacketDictionary() {
@@ -14,5 +19,13 @@ public class RTSPacketDictionary extends PacketDictionary {
         this.registerPacket(LoginResponse.class);
         this.registerPacket(RegistrationRequest.class);
         this.registerPacket(RegistrationResponse.class);
+
+        // Game lobby packets here
+        this.registerPacket(LobbyCreationRequest.class);
+        this.registerPacket(LobbyCreationResponse.class);
+        this.registerPacket(LobbyDeletionRequest.class);
+        this.registerPacket(LobbyListingRequest.class);
+        this.registerPacket(LobbyListingResponse.class);
     }
+
 }
