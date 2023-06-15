dc8e6f78564e90ef48b4d89ee66c502e3bd8bc23
masecla22 <supermasecla22@gmail.com>
2023-06-16 00:21:00 +0200

    handleBack is now called on main thread

--- 3_rts/game/src/main/java/nl/rug/oop/rts/Game.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/Game.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import nl.rug.oop.rts.controller.settings.SettingsController;
 import nl.rug.oop.rts.view.MainMenuClass;
 import nl.rug.oop.rts.view.View;
-import nl.rug.oop.rts.view.multiplayer.LoginView;
-import nl.rug.oop.rts.view.multiplayer.RegisterView;
 import nl.rug.oop.rts.view.settings.SettingsView;
 
 public class Game {
@@ -44,13 +42,19 @@ public class Game {
     }
 
     public void handleBack() {
+        if (!SwingUtilities.isEventDispatchThread()) {
+            SwingUtilities.invokeLater(this::handleBack);
+            return;
+        }
+
         if (this.accessedViews.size() > 0) {
             View view = this.accessedViews.remove(this.accessedViews.size() - 1);
             view.onClose();
         }
 
-        JPanel lastView = this.accessedViews.get(this.accessedViews.size() - 1);
+        View lastView = this.accessedViews.get(this.accessedViews.size() - 1);
         this.frame.setContentPane(lastView);
+        lastView.onOpen();
 
         this.frame.revalidate();
         this.frame.repaint();
@@ -60,19 +64,12 @@ public class Game {
         this.accessedViews.add(panel);
         this.frame.setContentPane(panel);
 
+        panel.onOpen();
         this.frame.revalidate();
         this.frame.repaint();
     }
 
     public void openSettings() {
         handleView(new SettingsView(this, this.settingsController));
-    }
-
-    public void handleLogin() {
-        handleView(new LoginView(this));
-    }
-
-    public void handleRegister() {
-        handleView(new RegisterView(this));
     }
 }
