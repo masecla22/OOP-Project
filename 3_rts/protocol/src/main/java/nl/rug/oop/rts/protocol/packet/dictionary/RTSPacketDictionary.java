fcb95d3b650862c54645926cd5fb8ff8454de1a4
masecla22 <supermasecla22@gmail.com>
2023-06-15 22:36:12 +0200

    Update RTSPacketDictionary.java

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/RTSPacketDictionary.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/dictionary/RTSPacketDictionary.java
@@ -1,9 +1,18 @@
 package nl.rug.oop.rts.protocol.packet.dictionary;
 
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.login.LoginRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.login.LoginResponse;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.register.RegistrationRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.register.RegistrationResponse;
+
 public class RTSPacketDictionary extends PacketDictionary {
     public RTSPacketDictionary() {
         super();
 
         // Register packets here
+        this.registerPacket(LoginRequest.class);
+        this.registerPacket(LoginResponse.class);
+        this.registerPacket(RegistrationRequest.class);
+        this.registerPacket(RegistrationResponse.class);
     }
 }
