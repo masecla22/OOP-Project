18c7fb93a97cdb9508e7f16d60c9063c947625ed
masecla22 <supermasecla22@gmail.com>
2023-06-16 00:44:45 +0200

    Create MultiplayerLobby.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/games/MultiplayerLobby.java
@@ -0,0 +1,23 @@
+package nl.rug.oop.rts.protocol.games;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.objects.model.Map;
+import nl.rug.oop.rts.protocol.user.User;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MultiplayerLobby {
+    private UUID lobbyUUID;
+
+    private Map map;
+    private String mapName;
+
+    private String name;
+
+    private User host;
+}
