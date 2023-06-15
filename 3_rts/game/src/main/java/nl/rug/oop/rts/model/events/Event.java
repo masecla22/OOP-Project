d6c920d1e6aa81bc04de7f9e9ad56a96f5b95a51
masecla22 <supermasecla22@gmail.com>
2023-06-15 13:15:34 +0200

    Create Event.java

---
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/model/events/Event.java
@@ -0,0 +1,13 @@
+package nl.rug.oop.rts.model.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nl.rug.oop.rts.model.armies.Army;
+
+@Getter
+@AllArgsConstructor
+public abstract class Event {
+    private EventType type;
+
+    public abstract void execute(Army army);
+}
