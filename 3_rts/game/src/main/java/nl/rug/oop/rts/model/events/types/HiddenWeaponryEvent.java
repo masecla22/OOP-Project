b484a9370aba995ffc90b5ad036a3b33d385c295
masecla22 <supermasecla22@gmail.com>
2023-06-15 13:15:38 +0200

    Create HiddenWeaponryEvent.java

---
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/model/events/types/HiddenWeaponryEvent.java
@@ -0,0 +1,16 @@
+package nl.rug.oop.rts.model.events.types;
+
+import nl.rug.oop.rts.model.armies.Army;
+import nl.rug.oop.rts.model.events.Event;
+import nl.rug.oop.rts.model.events.EventType;
+
+public class HiddenWeaponryEvent extends Event {
+    public HiddenWeaponryEvent() {
+        super(EventType.NATURAL_DISASTER);
+    }
+
+    @Override
+    public void execute(Army army) {
+        army.getUnits().forEach(unit -> unit.setDamage(unit.getDamage() + 1));
+    }
+}
