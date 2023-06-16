f2487d40ac45ab1f4496409616c2175e41da7862
masecla22 <supermasecla22@gmail.com>
2023-06-16 22:02:25 +0200

    Create GameScopedPacket.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/GameScopedPacket.java
@@ -0,0 +1,17 @@
+package nl.rug.oop.rts.protocol.packet.definitions.game;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.AuthenticatedPacket;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class GameScopedPacket extends AuthenticatedPacket {
+    private UUID gameId;
+}
