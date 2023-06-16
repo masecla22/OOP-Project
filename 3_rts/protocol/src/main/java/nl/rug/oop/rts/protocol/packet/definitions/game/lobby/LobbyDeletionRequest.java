f57b386f241594b6751b5cbcb7e16d6eb229abf3
masecla22 <supermasecla22@gmail.com>
2023-06-16 14:18:02 +0200

    Update LobbyDeletionRequest.java

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/lobby/LobbyDeletionRequest.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/lobby/LobbyDeletionRequest.java
@@ -1,17 +1,11 @@
 package nl.rug.oop.rts.protocol.packet.definitions.game.lobby;
 
-import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import nl.rug.oop.rts.protocol.packet.definitions.authentication.AuthenticatedPacket;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class LobbyDeletionRequest extends AuthenticatedPacket {
-    private UUID lobbyId;
+public class LobbyDeletionRequest extends LobbyScopedPacket{
 }
