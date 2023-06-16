0c4f990b55942060f404441fc3fc7e9175b25c4e
masecla22 <supermasecla22@gmail.com>
2023-06-16 14:17:58 +0200

    Update HandlerBinder.java

--- 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/HandlerBinder.java
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/HandlerBinder.java
@@ -2,11 +2,16 @@ package nl.rug.oop.rts.server.handlers;
 
 import lombok.AllArgsConstructor;
 import nl.rug.oop.rts.protocol.SocketConnection;
+import nl.rug.oop.rts.server.games.GamesManager;
 import nl.rug.oop.rts.server.handlers.authentication.AuthenticatedPacketHandler;
 import nl.rug.oop.rts.server.handlers.authentication.LoginHandler;
 import nl.rug.oop.rts.server.handlers.authentication.RegistrationHandler;
+import nl.rug.oop.rts.server.handlers.games.lobby.LobbyCreationRequestHandler;
+import nl.rug.oop.rts.server.handlers.games.lobby.LobbyDeletionRequestHandler;
 import nl.rug.oop.rts.server.handlers.games.lobby.LobbyListingRequestHandler;
+import nl.rug.oop.rts.server.handlers.games.lobby.LobbyScopedHandler;
 import nl.rug.oop.rts.server.main.RTSServer;
+import nl.rug.oop.rts.server.user.UserManager;
 
 @AllArgsConstructor
 public class HandlerBinder {
@@ -18,11 +23,18 @@ public class HandlerBinder {
     }
 
     private void registerAuthenticationListeners() {
-        connection.addListener(new LoginHandler(server.getUserManager()));
-        connection.addListener(new RegistrationHandler(server.getUserManager()));
+        UserManager userManager = server.getUserManager();
+        GamesManager gamesManager = server.getGamesManager();
 
-        connection.addListener(new AuthenticatedPacketHandler(server.getUserManager()));
+        connection.addListener(new LoginHandler(userManager));
+        connection.addListener(new RegistrationHandler(userManager));
 
-        connection.addListener(new LobbyListingRequestHandler(server.getGamesManager()));
+        connection.addListener(new AuthenticatedPacketHandler(userManager));
+        
+
+        connection.addListener(new LobbyScopedHandler(userManager, gamesManager));
+        connection.addListener(new LobbyListingRequestHandler(gamesManager));
+        connection.addListener(new LobbyCreationRequestHandler(gamesManager, userManager));
+        connection.addListener(new LobbyDeletionRequestHandler(gamesManager));
     }
 }
