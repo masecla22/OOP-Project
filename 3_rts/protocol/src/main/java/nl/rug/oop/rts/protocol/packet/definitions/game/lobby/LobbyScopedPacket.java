0b5bac80cd9ad0714364bc433d65cba037daf9b6
masecla22 <supermasecla22@gmail.com>
2023-06-16 14:17:42 +0200

    Create LobbyScopedPacket.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/lobby/LobbyScopedPacket.java
@@ -0,0 +1,18 @@
+package nl.rug.oop.rts.protocol.packet.definitions.game.lobby;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.AuthenticatedPacket;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class LobbyScopedPacket extends AuthenticatedPacket {
+    private UUID lobbyId;
+
+}
