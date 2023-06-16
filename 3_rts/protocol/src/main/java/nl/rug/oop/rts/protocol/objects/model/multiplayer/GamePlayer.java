33b5aff0b3e11dc07c813a2b1b39ba1296166f7b
masecla22 <supermasecla22@gmail.com>
2023-06-16 22:02:43 +0200

    Create GamePlayer.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/multiplayer/GamePlayer.java
@@ -0,0 +1,26 @@
+package nl.rug.oop.rts.protocol.objects.model.multiplayer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import nl.rug.oop.rts.protocol.SocketConnection;
+import nl.rug.oop.rts.protocol.objects.model.Node;
+import nl.rug.oop.rts.protocol.objects.model.armies.Team;
+import nl.rug.oop.rts.protocol.user.User;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+public class GamePlayer {
+    @NonNull
+    private User user;
+    private int gold = 0;
+
+    private Team team;
+    private Node startingNode;
+
+    private transient SocketConnection connection;
+}
