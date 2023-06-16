4e11d4613ead578822914f8b20b504a3e11755f0
masecla22 <supermasecla22@gmail.com>
2023-06-16 22:03:36 +0200

    Update KeepAlivePacket.java

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/keepalive/KeepAlivePacket.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/keepalive/KeepAlivePacket.java
@@ -1,6 +1,10 @@
 package nl.rug.oop.rts.protocol.packet.definitions.keepalive;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import nl.rug.oop.rts.protocol.packet.Packet;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class KeepAlivePacket extends Packet {
 }
