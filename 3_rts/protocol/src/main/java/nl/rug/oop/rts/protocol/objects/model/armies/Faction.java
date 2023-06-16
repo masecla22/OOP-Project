e3930d966c89e144782401edcebb7fa5f9a8f5b1
masecla22 <supermasecla22@gmail.com>
2023-06-17 01:49:43 +0200

    Factions now have a cost per army

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/armies/Faction.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/armies/Faction.java
@@ -9,23 +9,25 @@ import nl.rug.oop.rts.protocol.objects.model.units.UnitType;
 @Getter
 @AllArgsConstructor
 public enum Faction {
-    MEN("Men", Team.TEAM_A, "factionMen",
+    MEN("Men", Team.TEAM_A, "factionMen", 100,
             List.of(UnitType.GONDOR_SOLDIER, UnitType.ITHILIEN_RANGER, UnitType.TOWER_GUARD)),
-    ELVES("Elves", Team.TEAM_A, "factionElves",
+    ELVES("Elves", Team.TEAM_A, "factionElves", 100,
             List.of(UnitType.LORIEN_WARRIOR, UnitType.MIRKWOOD_ARCHER, UnitType.RIVENDELL_LANCER)),
-    DWARVES("Dwarves", Team.TEAM_A, "factionDwarves",
+    DWARVES("Dwarves", Team.TEAM_A, "factionDwarves", 100,
             List.of(UnitType.AXE_THROWER, UnitType.GUARDIAN, UnitType.PHALANX)),
-    MORDOR("Mordor", Team.TEAM_B, "factionMordor",
+    MORDOR("Mordor", Team.TEAM_B, "factionMordor", 100,
             List.of(UnitType.HARADRIM_ARCHER, UnitType.ORC_PIKEMAN, UnitType.ORC_WARRIOR)),
-    ISENGARD("Isengard", Team.TEAM_B, "factionIsengard",
+    ISENGARD("Isengard", Team.TEAM_B, "factionIsengard", 100,
             List.of(UnitType.URUK_CROSSBOW_MAN, UnitType.URUK_HAI, UnitType.WARG_RIDER)),
-    ROHAN("Rohan", Team.TEAM_B, "factionRohan",
+    ROHAN("Rohan", Team.TEAM_B, "factionRohan", 100,
             List.of(UnitType.ROHAN_ARCHER, UnitType.ROHAN_SPEARMAN, UnitType.ROHAN_SWORDSMAN));
 
     private String niceName;
     private Team team;
 
     private String texture;
+
+    private int cost;
 
     private List<UnitType> availableUnits;
 
