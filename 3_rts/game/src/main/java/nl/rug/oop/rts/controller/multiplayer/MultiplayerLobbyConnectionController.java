74b2de5cd9ec825ce7b54b59937e9a4191b453c9
masecla22 <supermasecla22@gmail.com>
2023-06-17 01:49:16 +0200

    unused import

--- 3_rts/game/src/main/java/nl/rug/oop/rts/controller/multiplayer/MultiplayerLobbyConnectionController.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/controller/multiplayer/MultiplayerLobbyConnectionController.java
@@ -2,7 +2,6 @@ package nl.rug.oop.rts.controller.multiplayer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import nl.rug.oop.rts.Game;
 import nl.rug.oop.rts.protocol.SocketConnection;
 import nl.rug.oop.rts.protocol.games.MultiplayerLobby;
 import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyScopedPacket;
