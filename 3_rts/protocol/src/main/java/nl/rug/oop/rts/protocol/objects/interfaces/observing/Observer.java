3bc915db5b2cf2841e97bf54d05c414cd2d60ef8
masecla22 <supermasecla22@gmail.com>
2023-06-17 02:29:53 +0200

    Observer now only called on EDT

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/interfaces/observing/Observer.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/interfaces/observing/Observer.java
@@ -1,6 +1,7 @@
 package nl.rug.oop.rts.protocol.objects.interfaces.observing;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  * Interface for observer objects.
@@ -10,6 +11,11 @@ public interface Observer {
      * Update the observer.
      */
     default void update() {
+        if (!SwingUtilities.isEventDispatchThread()) {
+            SwingUtilities.invokeLater(this::update);
+            return;
+        }
+
         if ((this instanceof JPanel)) {
             ((JPanel) this).revalidate();
             ((JPanel) this).repaint();
