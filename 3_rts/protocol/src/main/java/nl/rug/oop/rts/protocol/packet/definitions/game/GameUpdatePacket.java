abea77fd038d23059db109296bfdc555d4a6986f
masecla22 <supermasecla22@gmail.com>
2023-06-17 15:25:51 +0200

    Update GameUpdatePacket.java

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/GameUpdatePacket.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/GameUpdatePacket.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import nl.rug.oop.rts.protocol.objects.model.multiplayer.MultiplayerGame;
+import nl.rug.oop.rts.protocol.packet.Packet;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class GameUpdatePacket extends GameScopedPacket {
+public class GameUpdatePacket extends Packet {
     private MultiplayerGame currentGame;
 }
