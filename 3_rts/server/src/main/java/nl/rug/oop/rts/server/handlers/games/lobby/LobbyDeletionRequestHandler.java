a315f88cc2cbe89160cbbeda45fb0df7061ac83e
masecla22 <supermasecla22@gmail.com>
2023-06-18 14:51:03 +0200

    Update LobbyDeletionRequestHandler.java

--- 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/games/lobby/LobbyDeletionRequestHandler.java
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/games/lobby/LobbyDeletionRequestHandler.java
@@ -8,10 +8,19 @@ import nl.rug.oop.rts.protocol.listeners.PacketListener;
 import nl.rug.oop.rts.protocol.packet.definitions.game.lobby.LobbyDeletionRequest;
 import nl.rug.oop.rts.server.games.GamesManager;
 
+/**
+ * This class handles a lobby deletion request. This is sent by the client 
+ * when the host leaves the lobby.
+ */
 public class LobbyDeletionRequestHandler extends PacketListener<LobbyDeletionRequest> {
 
     private GamesManager gamesManager;
 
+    /**
+     * Create a new LobbyDeletionRequestHandler.
+     * 
+     * @param gamesManager - The games manager
+     */
     public LobbyDeletionRequestHandler(GamesManager gamesManager) {
         super(LobbyDeletionRequest.class);
 
