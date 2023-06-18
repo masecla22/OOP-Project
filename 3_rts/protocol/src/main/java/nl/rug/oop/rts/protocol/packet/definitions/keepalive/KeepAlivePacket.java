8d801921394d7e8bf60a6c06f49ce1f0a60c53cf
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:39:35 +0200

    KeepAlivePacket comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/keepalive/KeepAlivePacket.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/keepalive/KeepAlivePacket.java
@@ -4,6 +4,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import nl.rug.oop.rts.protocol.packet.Packet;
 
+/**
+ * This packet is sent by the client to the server and vice versa to keep the
+ * connection alive. Doesn't contain any data.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class KeepAlivePacket extends Packet {
