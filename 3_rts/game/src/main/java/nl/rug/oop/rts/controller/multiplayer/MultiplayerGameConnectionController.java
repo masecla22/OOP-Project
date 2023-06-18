f77f317ca7655433819bfe1ea2291602863067d6
masecla22 <supermasecla22@gmail.com>
2023-06-18 11:11:28 +0200

    Added handling for the game end

--- 3_rts/game/src/main/java/nl/rug/oop/rts/controller/multiplayer/MultiplayerGameConnectionController.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/controller/multiplayer/MultiplayerGameConnectionController.java
@@ -1,12 +1,16 @@
 package nl.rug.oop.rts.controller.multiplayer;
 
+import javax.swing.JOptionPane;
+
 import lombok.AllArgsConstructor;
+import nl.rug.oop.rts.Game;
 import nl.rug.oop.rts.controller.map.MultiplayerMapController;
 import nl.rug.oop.rts.protocol.SocketConnection;
 import nl.rug.oop.rts.protocol.listeners.AwaitPacketOnce;
 import nl.rug.oop.rts.protocol.listeners.PacketListener;
 import nl.rug.oop.rts.protocol.objects.model.multiplayer.MultiplayerGame;
 import nl.rug.oop.rts.protocol.packet.Packet;
+import nl.rug.oop.rts.protocol.packet.definitions.game.GameEndPacket;
 import nl.rug.oop.rts.protocol.packet.definitions.game.GameScopedPacket;
 import nl.rug.oop.rts.protocol.packet.definitions.game.GameUpdatePacket;
 import nl.rug.oop.rts.protocol.packet.definitions.game.changes.GameChangeListConfirm;
@@ -14,12 +18,14 @@ import nl.rug.oop.rts.protocol.packet.definitions.game.changes.GameChangeListPac
 
 @AllArgsConstructor
 public class MultiplayerGameConnectionController {
+
     private MultiplayerMapController mapController;
     private MultiplayerConnectionController connectionController;
-    private MultiplayerGame game;
+    private MultiplayerGame multiGame;
+    private Game game;
 
     public void sendGamePacket(GameScopedPacket packet) {
-        packet.setGameId(game.getGameId());
+        packet.setGameId(multiGame.getGameId());
         this.connectionController.sendAuthenticatedPacket(packet);
     }
 
@@ -34,7 +40,7 @@ public class MultiplayerGameConnectionController {
         awaiter.getAwaiting().thenAccept(c -> {
             mapController.getChanges().clear();
             // game.setPlayerATurn(!game.isPlayerATurn());
-            game.update();
+            multiGame.update();
         });
     }
 
@@ -49,7 +55,27 @@ public class MultiplayerGameConnectionController {
         });
     }
 
+    public void bindGameEndListener() {
+        connectionController.getConnection().addListener(new PacketListener<GameEndPacket>(GameEndPacket.class) {
+            @Override
+            protected boolean handlePacket(SocketConnection connection, GameEndPacket packet) throws Exception {
+                showGameEnding(packet.isWinner(), packet.getNewElo());
+                return true;
+            }
+        });
+    }
+
     public void unbindGameChangeListener() {
         connectionController.getConnection().removeListeners(GameUpdatePacket.class);
+    }
+
+    public void unbindGameEndListener() {
+        connectionController.getConnection().removeListeners(GameEndPacket.class);
+    }
+
+    public void showGameEnding(boolean won, int elo) {
+        JOptionPane.showMessageDialog(null,
+                "Game ended. You " + (won ? "won" : "lost") + "! You now have " + elo + " ELO");
+        game.handleBack();
     }
 }
