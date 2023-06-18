004bac54271f19f54837a62ff109aab765f7907e
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:42:35 +0200

    Update View.java
    comments

--- 3_rts/game/src/main/java/nl/rug/oop/rts/view/View.java
+++ 3_rts/game/src/main/java/nl/rug/oop/rts/view/View.java
@@ -2,12 +2,19 @@ package nl.rug.oop.rts.view;
 
 import javax.swing.JPanel;
 
+/**
+ * Abstract class for all views.
+ */
 public abstract class View extends JPanel {
+    /**
+     * Called when the view is closed.
+     */
     public void onClose() {
-
     }
 
+    /**
+     * Called when the view is opened.
+     */
     public void onOpen() {
-        
     }
 }
