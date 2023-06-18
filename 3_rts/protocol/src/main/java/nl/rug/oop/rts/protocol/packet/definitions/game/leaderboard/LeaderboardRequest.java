74e8f7f1f1000b7c438f747ab54902b4d7d75be0
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:39:54 +0200

    Update LeaderboardRequest.java
    comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/leaderboard/LeaderboardRequest.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/leaderboard/LeaderboardRequest.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import nl.rug.oop.rts.protocol.packet.definitions.authentication.AuthenticatedPacket;
 
+/**
+ * Request a list of lobbies.
+ */
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
