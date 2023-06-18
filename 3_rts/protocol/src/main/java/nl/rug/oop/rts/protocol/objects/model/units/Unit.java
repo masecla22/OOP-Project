88761cfea9fdb1df08084c31f94df3df6fd388d6
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:40:26 +0200

    Update Unit.java
    comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/units/Unit.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/units/Unit.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This class represents a unit.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,7 +18,19 @@ public abstract class Unit {
     private double damage;
     private double health;
 
+    /**
+     * Deal damage to a unit.
+     * 
+     * @param unit - the unit
+     * @return - the damage dealt
+     */
     public abstract double dealDamage(Unit unit);
     
+    /**
+     * Take damage from a unit.
+     * 
+     * @param unit - the unit
+     * @param damage - the damage
+     */
     public abstract void takeDamage(Unit unit, double damage);
 }
