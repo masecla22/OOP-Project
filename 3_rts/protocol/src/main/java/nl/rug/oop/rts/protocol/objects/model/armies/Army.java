be54e673498027cfe4dbc756832677613782fd82
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:41:49 +0200

    Update Army.java
    comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/armies/Army.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/armies/Army.java
@@ -9,25 +9,39 @@ import lombok.NonNull;
 import nl.rug.oop.rts.protocol.objects.model.Node;
 import nl.rug.oop.rts.protocol.objects.model.units.Unit;
 
+/**
+ * An army is a group of units.
+ */
 @Data
 @NoArgsConstructor
 public class Army {
     private List<Unit> units = new ArrayList<>();
     private Faction faction;
 
+    private transient Node movingToNextStep;
+    private transient boolean moved = false;
+
+    /**
+     * Create a new army.
+     * 
+     * @param units   - the units
+     * @param faction - the faction
+     */
     public Army(@NonNull List<Unit> units, @NonNull Faction faction) {
         this.units = units;
         this.faction = faction;
     }
-
-    private transient Node movingToNextStep;
-    private transient boolean moved = false;
 
     @Override
     public String toString() {
         return faction.toString() + " army (" + units.size() + " units)";
     }
 
+    /**
+     * Check if this army is empty.
+     * 
+     * @return - true if empty, false otherwise
+     */
     public boolean isEmpty() {
         return units.isEmpty();
     }
