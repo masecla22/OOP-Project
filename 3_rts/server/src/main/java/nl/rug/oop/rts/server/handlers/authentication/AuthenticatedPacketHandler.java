ab14bfb3ae22ab779d2fa038087a376a268c1aa5
masecla22 <supermasecla22@gmail.com>
2023-06-18 14:52:09 +0200

    Update AuthenticatedPacketHandler.java

--- 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/authentication/AuthenticatedPacketHandler.java
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/authentication/AuthenticatedPacketHandler.java
@@ -6,9 +6,18 @@ import nl.rug.oop.rts.protocol.packet.definitions.authentication.AuthenticatedPa
 import nl.rug.oop.rts.protocol.user.User;
 import nl.rug.oop.rts.server.user.UserManager;
 
+/**
+ * This class handles an authenticated packet. It will ensure the session token
+ * is valid, and if it is, it will pass the packet on to the next handler.
+ */
 public class AuthenticatedPacketHandler extends PacketListener<AuthenticatedPacket> {
     private UserManager userManager;
 
+    /**
+     * Create a new AuthenticatedPacketHandler.
+     * 
+     * @param userManager - The user manager
+     */
     public AuthenticatedPacketHandler(UserManager userManager) {
         super(AuthenticatedPacket.class);
         this.userManager = userManager;
@@ -17,8 +26,9 @@ public class AuthenticatedPacketHandler extends PacketListener<AuthenticatedPack
     @Override
     protected boolean handlePacket(SocketConnection connection, AuthenticatedPacket packet) {
         User user = userManager.getUser(packet.getSessionToken());
-        if (user == null)
+        if (user == null) {
             return false;
+        }
         return true;
     }
 
