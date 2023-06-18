d99e3d0d79928e4dc9aee31df636fb78629ce352
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:39:50 +0200

    Update LobbyCreationResponse.java
    comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/lobby/LobbyCreationResponse.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/game/lobby/LobbyCreationResponse.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 import nl.rug.oop.rts.protocol.games.MultiplayerLobby;
 import nl.rug.oop.rts.protocol.packet.Packet;
 
+/**
+ * Response to a lobby creation request.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class LobbyCreationResponse extends Packet {
     private boolean success;
-    
+
     private MultiplayerLobby lobby;
 }
