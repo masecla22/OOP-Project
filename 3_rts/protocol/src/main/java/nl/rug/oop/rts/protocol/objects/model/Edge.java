2c94bc736a0c00a2b895ba4e6a5e8df1d76eebe0
masecla22 <supermasecla22@gmail.com>
2023-06-18 13:41:42 +0200

    Update Edge.java
    comments

--- 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/Edge.java
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/objects/model/Edge.java
@@ -11,6 +11,9 @@ import nl.rug.oop.rts.protocol.objects.interfaces.Selectable;
 import nl.rug.oop.rts.protocol.objects.model.armies.Army;
 import nl.rug.oop.rts.protocol.objects.model.events.Event;
 
+/**
+ * An edge is a connection between two nodes.
+ */
 @Data
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
@@ -20,35 +23,74 @@ public class Edge implements Selectable {
     private Node pointA;
     private Node pointB;
 
+    private List<Army> armies = new ArrayList<>();
+    private List<Event> events = new ArrayList<>();
+
+    /**
+     * Create a new edge.
+     * 
+     * @param id     - the id
+     * @param pointA - the first node
+     * @param pointB - the second node
+     */
     public Edge(int id, Node pointA, Node pointB) {
         this.id = id;
         this.pointA = pointA;
         this.pointB = pointB;
     }
 
-    private List<Army> armies = new ArrayList<>();
-    private List<Event> events = new ArrayList<>();
-
+    /**
+     * Check if this edge is connected to a node.
+     * 
+     * @param node - the node
+     * @return - true if connected, false otherwise
+     */
     public boolean isConnectedTo(Node node) {
         return pointA.equals(node) || pointB.equals(node);
     }
 
+    /**
+     * Add an army to this edge.
+     * 
+     * @param army - the army
+     */
     public void addArmy(Army army) {
         armies.add(army);
     }
 
+    /**
+     * Remove an army from this edge.
+     * 
+     * @param army - the army
+     */
     public void removeArmy(Army army) {
         armies.remove(army);
     }
 
+    /**
+     * Add an event to this edge.
+     * 
+     * @param event - the event
+     */
     public void addEvent(Event event) {
         events.add(event);
     }
 
+    /**
+     * Remove an event from this edge.
+     * 
+     * @param event - the event
+     */
     public void removeEvent(Event event) {
         events.remove(event);
     }
 
+    /**
+     * Get the other node connected to this edge.
+     * 
+     * @param node - the node
+     * @return - the other node
+     */
     public Node getOtherNode(Node node) {
         if (pointA.equals(node)) {
             return pointB;
