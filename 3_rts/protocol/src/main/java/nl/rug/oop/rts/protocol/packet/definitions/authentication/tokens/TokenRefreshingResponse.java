4eea998c538974d55796cc1eaecf5237f5a1d493
masecla22 <supermasecla22@gmail.com>
2023-06-23 14:55:40 +0200

    Create TokenRefreshingResponse.java

---
+++ 3_rts/protocol/src/main/java/nl/rug/oop/rts/protocol/packet/definitions/authentication/tokens/TokenRefreshingResponse.java
@@ -0,0 +1,21 @@
+package nl.rug.oop.rts.protocol.packet.definitions.authentication.tokens;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nl.rug.oop.rts.protocol.packet.Packet;
+
+/**
+ * This packet is used to respond to a token refreshing request.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class TokenRefreshingResponse extends Packet {
+    private boolean success;
+    private UUID token;
+}
