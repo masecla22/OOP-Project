e67959dcd60db2a0583f99e2fa2f8412727e083f
masecla22 <supermasecla22@gmail.com>
2023-06-23 14:55:42 +0200

    Create RefreshTokenRequestHandler.java

---
+++ 3_rts/server/src/main/java/nl/rug/oop/rts/server/handlers/authentication/RefreshTokenRequestHandler.java
@@ -0,0 +1,41 @@
+package nl.rug.oop.rts.server.handlers.authentication;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import nl.rug.oop.rts.protocol.SocketConnection;
+import nl.rug.oop.rts.protocol.listeners.PacketListener;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.tokens.TokenRefreshingRequest;
+import nl.rug.oop.rts.protocol.packet.definitions.authentication.tokens.TokenRefreshingResponse;
+import nl.rug.oop.rts.server.user.UserManager;
+
+/**
+ * This class handles all requests for refreshing tokens.
+ */
+public class RefreshTokenRequestHandler extends PacketListener<TokenRefreshingRequest> {
+
+    private UserManager userManager;
+
+    public RefreshTokenRequestHandler(UserManager userManager) {
+        super(TokenRefreshingRequest.class);
+        this.userManager = userManager;
+    }
+
+    @Override
+    protected boolean handlePacket(SocketConnection connection, TokenRefreshingRequest packet)
+            throws IOException, SQLException {
+        UUID sessionToken = userManager.login(packet.getRefreshToken());
+
+        if (sessionToken == null) {
+            TokenRefreshingResponse response = new TokenRefreshingResponse(false, null);
+            connection.sendPacket(response);
+            return true;
+        }
+
+        TokenRefreshingResponse response = new TokenRefreshingResponse(true, sessionToken);
+        connection.sendPacket(response);
+        return true;
+    }
+
+}
